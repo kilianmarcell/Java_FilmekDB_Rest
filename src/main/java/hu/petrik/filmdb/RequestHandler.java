5f8446d0b312ee8a3bf3e50c636059721dc96464
kilianmarcell <kilian.marcell.andras@gmail.com>
2022-02-03 11:51:28 +0100

    post metódus kész

--- src/main/java/hu/petrik/filmdb/RequestHandler.java
+++ src/main/java/hu/petrik/filmdb/RequestHandler.java
@@ -1,49 +1,41 @@
 package hu.petrik.filmdb;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 public class RequestHandler {
     private RequestHandler() {}
     public static Response get(String url) throws IOException {
-        URL urlObj = new URL(url);
-        HttpURLConnection conn = (HttpURLConnection) urlObj.openConnection();
-        conn.setRequestProperty("Accept", "application/json");
-        conn.setConnectTimeout(10000);
-        conn.setReadTimeout(10000);
-
-        int responseCode = conn.getResponseCode();
-        InputStream is;
-        if (responseCode < 400) {
-            is = conn.getInputStream();
-        } else {
-            is = conn.getErrorStream();
-        }
-
-        is = conn.getInputStream();
-        StringBuilder builder = new StringBuilder();
-        BufferedReader br = new BufferedReader(new InputStreamReader(is));
-        String sor = br.readLine();
-        while (sor != null) {
-            builder.append(sor);
-            sor = br.readLine();
-        }
-        br.close();
-        is.close();
-        return new Response(responseCode, builder.toString());
+        HttpURLConnection conn = setupConnection(url);
+        return getResponse(conn);
     }
 
-    public static Response get(String url) throws IOException {
+    public static Response post(String url, String data) throws IOException {
+        HttpURLConnection conn = setupConnection(url);
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-Type", "application/json");
+        conn.setDoOutput(true);
+        OutputStream os = conn.getOutputStream();
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, StandardCharsets.UTF_8));
+        writer.write(data);
+        writer.flush();
+        writer.close();
+
+        return getResponse(conn);
+    }
+
+    private static HttpURLConnection setupConnection(String url) throws IOException {
         URL urlObj = new URL(url);
         HttpURLConnection conn = (HttpURLConnection) urlObj.openConnection();
         conn.setRequestProperty("Accept", "application/json");
         conn.setConnectTimeout(10000);
         conn.setReadTimeout(10000);
+        return conn;
+    }
 
+    private static Response getResponse(HttpURLConnection conn) throws IOException {
         int responseCode = conn.getResponseCode();
         InputStream is;
         if (responseCode < 400) {
