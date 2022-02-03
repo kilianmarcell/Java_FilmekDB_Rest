761bb40af5e7290b0829191338918684071a2199
kilianmarcell <kilian.marcell.andras@gmail.com>
2022-02-03 11:16:41 +0100

    RequestHandler olvasás kész

--- src/main/java/hu/petrik/filmdb/RequestHandler.java
+++ src/main/java/hu/petrik/filmdb/RequestHandler.java
@@ -1,14 +1,28 @@
 package hu.petrik.filmdb;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.Buffer;
 
 public class RequestHandler {
     private RequestHandler() {}
     public static void get(String url) throws IOException {
         URL urlObj = new URL(url);
         HttpURLConnection conn = (HttpURLConnection) urlObj.openConnection();
-
+        conn.setRequestProperty("Accept", "application/json");
+        conn.setConnectTimeout(10000);
+        conn.setReadTimeout(10000);
+        InputStream is = conn.getInputStream();
+        StringBuilder builder = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        String sor = br.readLine();
+        while (sor != null) {
+            builder.append(sor);
+            sor = br.readLine();
+        }
     }
 }
